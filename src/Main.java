751d14e6ca845a345b9ac6b62aa9d83db70b18ee
Jdunn1998 <dunn2jm@mail.uc.edu>
2025-02-09 16:23:56 -0500

    Finished Variables

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Double variable for the cost of lunch
+        double myLunchCost = 12.50;
+
+        //Interger variable for the number of kids in a family
+        int numberOfKids = 3;
+
+        //Boolean variable for chekcing if it's raining
+        boolean isRaining = false;
+
+        //Double Variable for the price of gas
+        double myGasPrice = 4.12;
+
+        //Interger variable for your favorite number
+        int favNumber = 27;
+
+        //Double variable for your shoe size
+        double shoeSize = 11.50;
+
+        //Integer variabel for your birth month
+        int birthMonth = 8;
+
+        // String variable for my name
+        String myName = "Jason Dunn";
+
+        //Outputs
+        System.out.println("The cost of my lunch is: $" + myLunchCost);
+        System.out.println("The number of kids in my family is: " + numberOfKids);
+        System.out.println("Is it raining: " + isRaining);
+        System.out.println("The price of gas is: $" + myGasPrice);
+        System.out.println("My favorite number is: " + favNumber);
+        System.out.println("My shoe size is: " + shoeSize);
+        System.out.println("My birth month is: " + birthMonth);
+        System.out.println("My name is: " + myName);
+
+
+
+
+
+
+
+    }
+}
